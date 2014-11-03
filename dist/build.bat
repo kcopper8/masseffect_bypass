ce14ec0ac7a68115d134331d788c8fad40df547d
kcopper8 <kcopper8@gmail.com>
2014-11-04 01:32:04 +0900

    heroku 배포용 빌드

--- dist/build.bat
+++ dist/build.bat
@@ -3,5 +3,8 @@ copy /Y src\css\ output\bypass\hacking\css\
 copy /Y src\img\ output\bypass\hacking\img\
 copy /Y src\js\config.js output\bypass\hacking\js\
 copy /Y src\js\lib\require.js output\bypass\hacking\js\lib\
+
+copy /Y src\base.html output\bypass\hacking\index.html
+
 cd dist
 node r.js -o buildconfig.js
--- src/js/controller/prizeController.js
+++ src/js/controller/prizeController.js
@@ -7,15 +7,22 @@ define(['app/config', 'lib/query-string'], function (Config, queryString) {
     parsedQueryString.success_url = parsedQueryString.success_url || Config.DefaultPrizeLocation;
     parsedQueryString.failure_url = parsedQueryString.failure_url || parsedQueryString.success_url || Config.DefaultPrizeLocation;
 
-    var prizeController = function () {
+    var prizeController = function (parsedQueryString) {
+        function noredirect() {
+            return !! parsedQueryString.noredirect;
+        }
         this.moveToSuccessTarget = function () {
-            location.href = parsedQueryString.success_url;
+            if (!noredirect()) {
+                location.href = parsedQueryString.success_url;
+            }
         };
 
         this.moveToFailedTarget = function () {
-            location.href = parsedQueryString.failure_url;
+            if (!noredirect()) {
+                location.href = parsedQueryString.failure_url;
+            }
         };
     };
 
-    return new prizeController();
+    return new prizeController(parsedQueryString);
 });
