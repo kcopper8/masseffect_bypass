1fc5989ce4280860f061102ac1fe7d85a1ee54aa
kcopper8 <kcopper8@gmail.com>
2014-11-03 21:35:26 +0900

    cursor html 분리

--- src/js/bui/cursor.js
+++ src/js/bui/cursor.js
@@ -1,17 +1,11 @@
 /**
  * Created by user on 2014-10-18.
  */
-define(['jquery', 'backbone', 'bui/card'], function ($, Backbone) {
-    var template = [
-        '<img class="bp_left" src="img/cursor_left.png">',
-        '<img class="bp_arrow" src="img/cursor_arrow.png">',
-        '<img class="bp_right" src="img/cursor_right.png">'
-    ].join('');
-
+define(['jquery', 'backbone', 'text!bui/template/cursorTemplate.html', 'bui/card'], function ($, Backbone, cursorTemplate) {
     var Cursor = Backbone.View.extend({
         className : "bp_cursor",
         initialize : function () {
-            this.$el.html(template);
+            this.$el.html(cursorTemplate);
         },
         show : function () {
             this.$el.show();
