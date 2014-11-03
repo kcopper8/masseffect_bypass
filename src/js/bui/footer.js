d3231f211ee52e82a4ddb2e828ec53ccc61f0a70
kcopper8 <kcopper8@gmail.com>
2014-11-03 21:40:28 +0900

    footer html 분리

--- src/js/bui/footer.js
+++ src/js/bui/footer.js
@@ -1,18 +1,22 @@
 /**
  * Created by user on 2014-11-02.
  */
-define(['backbone', 'jquery'], function (Backbone, $) {
+define(['backbone', 'jquery', 'text!bui/template/footerTemplate.html'], function (Backbone, $, footerTemplate) {
     var Footer = Backbone.View.extend({
-        initialize : function () {
-            this.$remainCodeCounter = this.$(".bp_remain_code_count");
-            this.listenTo(this.model, 'change', this.render);
-            this.render();
-        },
+        template : _.template(footerTemplate),
         events : {
             'click .bp_exit A' : function (e) {
                 e.preventDefault();
                 this.trigger("exit");
             }
+        },
+        className : "bp_footer",
+
+        initialize : function () {
+            this.$el.html(this.template(this.model.toJSON()));
+            this.$remainCodeCounter = this.$(".bp_remain_code_count");
+            this.listenTo(this.model, 'change', this.render);
+            this.render();
         },
         render : function () {
             this.$remainCodeCounter.html(this.model.getRemainCodeCount());
