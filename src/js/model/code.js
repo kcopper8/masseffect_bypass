229ad1f3b45688788d87b51fe8e5f3ec6aac3647
kcopper8 <kcopper8@gmail.com>
2014-11-11 03:16:11 +0900

    코드 이미지 변경

--- src/js/model/code.js
+++ src/js/model/code.js
@@ -10,7 +10,7 @@ define(['app/config', 'underscore'], function (Config, _) {
         this.number = number;
 
         this.getPath = function() {
-            return Config.CodePathPrefix + this.number + ".svg";
+            return Config.CodePathPrefix + this.number + ".png";
         };
     };
 
