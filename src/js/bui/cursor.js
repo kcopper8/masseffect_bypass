ce2b65461e4ad81ab57ce4a42991a45f1e1bfb85
kcopper8 <kcopper8@gmail.com>
2014-11-15 00:28:43 +0900

    css 정리

--- src/js/bui/cursor.js
+++ src/js/bui/cursor.js
@@ -1,12 +1,9 @@
 /**
  * Created by user on 2014-10-18.
  */
-define(['jquery', 'backbone', 'text!bui/template/cursorTemplate.html', 'bui/card'], function ($, Backbone, cursorTemplate) {
+define(['backbone'], function (Backbone) {
     var Cursor = Backbone.View.extend({
         className : "bp_cursor",
-        initialize : function () {
-            this.$el.html(cursorTemplate);
-        },
         show : function () {
             this.$el.show();
         },
